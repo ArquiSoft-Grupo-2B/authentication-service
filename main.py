1b037dc97c954d328380d9b85609a4b2646f8450
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2025-10-01 10:39:06 -0500

    fix: enabling cors from all origins

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 from src.infraestructure.graphql.schema import schema
 from src.infraestructure.graphql.context import get_context
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 graphql_app = GraphQLRouter(schema, context_getter=get_context)
 app.include_router(graphql_app, prefix="/graphql")
 
